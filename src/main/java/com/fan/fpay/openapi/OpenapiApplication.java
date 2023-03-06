2582c6c038210e8855509450b635f556010980ec
jianbo <jianbo.fan2016@gmail.com>
2023-03-06 22:45:11 +0800

    init main class

---
+++ src/main/java/com/fan/fpay/openapi/OpenapiApplication.java
@@ -0,0 +1,14 @@
+package com.fan.fpay.openapi;
+
+/**
+ * @Author jianbo
+ * @Date 2023/3/6 22:44
+ * @Version 1.0
+ * @Description <br/>
+ *
+ */
+public class OpenapiApplication {
+    public static void main(String[] args) {
+
+    }
+}
